d2003b1dbd1e3d207ecf9e4d096313ad7b73774c
Orion Ford <orion.ford@hq.vertique.com>
2023-09-01 15:44:41 -0400

    adding files

---
+++ first-app-lesson-01/src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+  <section class="results">
+    <app-housing-location></app-housing-location>
+  </section>
+  `,
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent {
+
+}
